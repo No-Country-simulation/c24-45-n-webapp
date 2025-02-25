b733bbdc4dc23b0096d178aac9a4c48ebc6351a3
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2025-02-24 19:45:09 -0600

    Creacion del registro de organizaciones

--- Backend/GivenHands/src/main/java/com/no_country/GivenHands/service/RegisterUserService.java
+++ Backend/GivenHands/src/main/java/com/no_country/GivenHands/service/RegisterUserService.java
@@ -2,6 +2,7 @@ package com.no_country.GivenHands.service;
 
 import com.no_country.GivenHands.dto.RegisterUserDto;
 import com.no_country.GivenHands.model.RegisterUser;
+import com.no_country.GivenHands.model.enumeration.Rol;
 import com.no_country.GivenHands.repository.RegisterUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class RegisterUserService {
         newRegisterUser.setUserName(registerUserDto.userName());
         newRegisterUser.setEmail(registerUserDto.email());
         newRegisterUser.setPassword(registerUserDto.password());
+        newRegisterUser.setRol(Rol.VOLUNTEER);
         registerUserRepository.save(newRegisterUser);
     }
     public void validationRegisterUser(String userName, String email, String password, String password2)throws IllegalArgumentException{
