40432c26b1c6d51f2277518b8b1713ae1e24f3e6
joams mogro <joamsmg@gmail.com>
2025-03-02 00:59:22 -0400

    fix: custom button was removed

--- Frontend/src/app/features/home/components/start-now/start-now.component.ts
+++ Frontend/src/app/features/home/components/start-now/start-now.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
-import {
-  GetStartedNowButtonComponent,
-  SizeType,
-} from '../../../../shared/get-started-now-button/get-started-now-button.component';
 
 @Component({
   selector: 'start-now',
-  imports: [GetStartedNowButtonComponent],
+  imports: [],
   templateUrl: './start-now.component.html',
   styleUrl: './start-now.component.css',
 })
-export class StartNowComponent {
-  SizeType = SizeType;
-}
+export class StartNowComponent {}
