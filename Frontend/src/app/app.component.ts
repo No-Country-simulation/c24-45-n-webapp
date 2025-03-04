1545d69119d422562b2e2c9442a1347fde9c520a
Juancaro1 <carojuan179@gmail.com>
2025-03-04 13:30:24 -0300

    guardado antes del pull de develop

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./features/auth/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend';
+
+  constructor(private router : Router) {
+    console.log('AppComponent constructor');
+  }
+
+  login(ruta : string) {
+    this.router.navigate(["/" + ruta]);
+  }
 }
