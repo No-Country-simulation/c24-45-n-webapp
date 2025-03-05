66652966ff1f2ffab0919165620a86f82fe11f41
Kevin <kev3334@gmail.com>
2025-03-05 15:59:36 -0500

    se restructura las entidades y relaciones de los registros de usuarios

--- Backend/GivenHands/src/main/java/com/no_country/GivenHands/controller/RegisterVolunteerController.java
+++ Backend/GivenHands/src/main/java/com/no_country/GivenHands/controller/RegisterVolunteerController.java
@@ -1,7 +1,7 @@
 package com.no_country.GivenHands.controller;
 
-import com.no_country.GivenHands.dto.RegisterUserDto;
-import com.no_country.GivenHands.service.RegisterUserService;
+import com.no_country.GivenHands.dto.RegisterVolunteerDTO;
+import com.no_country.GivenHands.service.RegisterVolunteerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/userregister")
-public class RegisterUserController {
+@RequestMapping("/volunteer")
+public class RegisterVolunteerController {
     @Autowired
-    private RegisterUserService registerUserService;
+    private RegisterVolunteerService registerVolunteerService;
 
     @PostMapping("/register")
-    public ResponseEntity regiterUser(@RequestBody RegisterUserDto registerUserDto) {
+    public ResponseEntity regiterUser(@RequestBody RegisterVolunteerDTO registerVolunteerDTO) {
         try {
-            registerUserService.regitrarUsuario(registerUserDto);
-            return ResponseEntity.ok("Usuario Registrado!!");
+            registerVolunteerService.regiterUserVolunteer(registerVolunteerDTO);
+            return ResponseEntity.ok("Usuario Voluntario Registrado!!");
         } catch (Exception e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
--- Backend/GivenHands/src/main/java/com/no_country/GivenHands/model/Organization.java
+++ Backend/GivenHands/src/main/java/com/no_country/GivenHands/model/Organization.java
@@ -1,45 +1,45 @@
 package com.no_country.GivenHands.model;
 
 import com.no_country.GivenHands.model.enumeration.Cause;
-import com.no_country.GivenHands.model.enumeration.Rol;
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Table(name = "organizations")
+@Table(name = "Organization")
 public class Organization {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true)
-    private String username;
-    @Column(nullable = false, unique = true)
-    private String email;
-    @Column(nullable = false)
-    private String password;
-    @Column (nullable = false, unique = true)
+
+    @OneToOne
+    @JoinColumn(name = "register_user_id", nullable = false)
+    private RegisterUser registerUser;
+
+    @Column (nullable = false)
     private String name;
+
     @Column (nullable = false)
     private String description;
+
     @Enumerated(EnumType.STRING)
     @Column (nullable = false)
     private Cause cause;
+
     private Address address;
     @Column (nullable = false)
+
     private int phone;
+
     private String webSite;
+
     private String socialMedia;
-    @Enumerated(EnumType.STRING)
-    private Rol rol;
+
 
     public Organization() {
     }
 
-    public Organization(Long id, String username, String email, String password, String name, String description, Cause cause, Address address, int phone, String webSite, String socialMedia, Rol rol) {
+    public Organization(Long id, RegisterUser registerUser, String name, String description, Cause cause, Address address, int phone, String webSite, String socialMedia) {
         this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
+        this.registerUser = registerUser;
         this.name = name;
         this.description = description;
         this.cause = cause;
@@ -47,7 +47,6 @@ public class Organization {
         this.phone = phone;
         this.webSite = webSite;
         this.socialMedia = socialMedia;
-        this.rol = rol;
     }
 
     public Long getId() {
@@ -58,28 +57,12 @@ public class Organization {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public RegisterUser getRegisterUser() {
+        return registerUser;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setRegisterUser(RegisterUser registerUser) {
+        this.registerUser = registerUser;
     }
 
     public String getName() {
@@ -136,31 +119,5 @@ public class Organization {
 
     public void setSocialMedia(String socialMedia) {
         this.socialMedia = socialMedia;
-    }
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-
-    @Override
-    public String toString() {
-        return "Organization{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", cause=" + cause +
-                ", address=" + address +
-                ", phone=" + phone +
-                ", webSite='" + webSite + '\'' +
-                ", socialMedia='" + socialMedia + '\'' +
-                ", rol=" + rol +
-                '}';
     }
 }
