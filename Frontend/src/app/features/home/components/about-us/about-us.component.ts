445358174f5d09dae37e304634dfb2f603359f2c
joams mogro <joamsmg@gmail.com>
2025-03-06 18:26:34 -0400

    feat: join us button was updated with a route

--- Frontend/src/app/features/home/components/about-us/about-us.component.ts
+++ Frontend/src/app/features/home/components/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'about-us',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css',
 })
 export class AboutUsComponent {
+
+  router = inject(Router)
+
+  goToRegister(){
+    this.router.navigate(['/register'],{replaceUrl:true})
+  }
 }
