8016cf65f7d583b0ba7bc05ed537dc381423bb48
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2025-03-06 16:14:49 -0600

    Listar organizaciones

---
+++ Backend/GivenHands/src/main/java/com/no_country/GivenHands/controller/OrganizationController.java
@@ -0,0 +1,40 @@
+package com.no_country.GivenHands.controller;
+
+import com.no_country.GivenHands.dto.OrganizationDTO;
+import com.no_country.GivenHands.service.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/organization")
+public class OrganizationController {
+    @Autowired
+    private OrganizationService organizationService;
+
+    // Listar todas las organizaciones
+    @GetMapping
+    public ResponseEntity<List<OrganizationDTO>> getAllOrganizations() {
+        List<OrganizationDTO> organizations = organizationService.getAllOrganizations();
+        return ResponseEntity.ok(organizations);
+    }
+
+    // Buscar organizacion por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOrganizationById(@PathVariable Long id) {
+        Optional<OrganizationDTO> organization = organizationService.getOrganizationById(id);
+        if (organization != null) {
+            return ResponseEntity.ok(organization);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro la organizacion del id: " + id);
+        }
+    }
+}
+
