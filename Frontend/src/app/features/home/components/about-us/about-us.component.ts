5419f660796cfd7913ebc83d28d4f25e756fefb3
joams mogro <joamsmg@gmail.com>
2025-03-06 00:48:14 -0400

    fix: get started button was changed by a button tag

--- Frontend/src/app/features/home/components/about-us/about-us.component.ts
+++ Frontend/src/app/features/home/components/about-us/about-us.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import {
-  GetStartedNowButtonComponent,
-  SizeType,
-} from '../../../../shared/get-started-now-button/get-started-now-button.component';
 
 @Component({
   selector: 'about-us',
-  imports: [GetStartedNowButtonComponent],
+  imports: [],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css',
 })
 export class AboutUsComponent {
-  SizeType = SizeType;
 }
