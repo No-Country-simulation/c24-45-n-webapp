f02c02e603c2f390f4d29c0b523cb8e0f0f39870
joams mogro <joamsmg@gmail.com>
2025-03-09 23:55:43 -0400

    feat: some imports were removed

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -2,21 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { LoginComponent } from './features/auth/login/login.component';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './features/home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RegisterComponent } from './features/auth/register/register.component';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
-    LoginComponent,
-    AppComponent,
-    HomeComponent,
-    ReactiveFormsModule,
-    AppComponent,
-    RegisterComponent,
   ],
 };
