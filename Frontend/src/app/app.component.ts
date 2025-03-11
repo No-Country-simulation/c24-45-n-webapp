b623d2c43c6f1b69fc4094bc1229558be25c3e72
GenesisBaquerizo <gdbaquer@gmail.com>
2025-03-11 16:44:12 -0500

    formulario voluntarios funcional

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angula
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule,VolunteerFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/volunteer-form/volunteer-form.component.ts
+++ Frontend/src/app/volunteer-form/volunteer-form.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-volunteer-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './volunteer-form.component.html',
   styleUrl: './volunteer-form.component.css'
 })
 export class VolunteerFormComponent implements OnInit {
-  miFormulario!: FormGroup; // Define miFormulario
+  miFormulario!: FormGroup; 
   avatarPreview: string | ArrayBuffer | null = null;
   defaultAvatar = './assets/avatar.jpg';
+  formSubmitted = false;
 
   constructor(private fb: FormBuilder, private router: Router) {}
 
@@ -30,26 +32,31 @@ export class VolunteerFormComponent implements OnInit {
   ngOnInit() {
     this.avatarPreview = this.defaultAvatar;
     this.miFormulario = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
+      names: ['', Validators.required],
+      surnames: ['', Validators.required],
       birthday: ['', Validators.required],
       country: ['', Validators.required],
       province: ['', Validators.required],
       city: ['', Validators.required],
-      volunteer_preference: ['', Validators.required],
-      skill: ['', Validators.required],
+      volunteerPreference: ['', Validators.required],
+      skills: ['', Validators.required],
       phone: ['', Validators.required],
-      adress: ['', Validators.required],
-      zip_code: ['', Validators.required],
+      address: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      avatar: [null],
     });
   }
 
-  onSubmit() { // Define onSubmit()
+  onSubmit() { 
+    this.formSubmitted = true;
     if (this.miFormulario.valid) {
+      console.log( this.formSubmitted);
       console.log(this.miFormulario.value);
+      this.router.navigate(['/muro']);
+    }
+    else {
+      console.log('Formulario inválido');
     }
   }
-  irMuro() {
-    this.router.navigate(['/muro']);
-  }
+
 }
